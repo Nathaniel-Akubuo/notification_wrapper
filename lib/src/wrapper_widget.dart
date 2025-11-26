aea7cd6aa4e825e3808e2d1348ad9354068f2971
Nathaniel Akubuo <nakubuo@gmail.com>
2025-11-26 07:35:57 +0100

    add jpush as a notiifcation service

--- lib/src/wrapper_widget.dart
+++ lib/src/wrapper_widget.dart
@@ -1,8 +1,10 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/widgets.dart';
-import 'package:notification_wrapper/src/typedefs.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:jpush_flutter/jpush_interface.dart';
 
 import 'helper_functions.dart';
+import 'typedefs.dart';
 
 class NotificationWrapperWidget extends StatefulWidget {
   final Widget child;
@@ -18,6 +20,7 @@ class NotificationWrapperWidget extends StatefulWidget {
 
   final String? androidSoundFile;
   final String? channelKey;
+  final String? jpushAppKey;
 
   const NotificationWrapperWidget({
     super.key,
@@ -31,6 +34,7 @@ class NotificationWrapperWidget extends StatefulWidget {
     this.onGetToken,
     this.androidSoundFile,
     this.channelKey,
+    this.jpushAppKey,
   });
 
   @override
@@ -40,54 +44,112 @@ class NotificationWrapperWidget extends StatefulWidget {
 
 class _NotificationWrapperWidgetState extends State<NotificationWrapperWidget> {
   final _fcm = FirebaseMessaging.instance;
+  final _jPush = JPush.newJPush();
 
   @override
   void initState() {
     super.initState();
-
-    _listenForNotifications();
+    _init();
   }
 
-  Future<void> _listenForNotifications() async {
+  Future<void> _init() async {
     await HelperFunctions.setupLocalNotifications(
       widget.onTap,
       widget.androidSoundFile,
       widget.channelKey,
     );
+    if (widget.jpushAppKey != null) await _initJPush();
+
+    await _initFCM();
+  }
+
+  Future<void> _initFCM() async {
     await HelperFunctions.setIOSOptions();
-    var token = await _fcm.getToken();
+
+    final token = await _fcm.getToken();
     widget.onGetToken?.call(token);
 
-    RemoteMessage? initialMessage = await _fcm.getInitialMessage();
-
+    final initialMessage = await _fcm.getInitialMessage();
     if (initialMessage != null) {
       widget.onNotificationReceived?.call(initialMessage);
     }
 
-    FirebaseMessaging.onMessageOpenedApp.listen((event) {
-      widget.onNotificationReceived?.call(event);
+    FirebaseMessaging.onMessageOpenedApp.listen((msg) {
+      widget.onNotificationReceived?.call(msg);
+      widget.onTap?.call(msg.data);
     });
 
-    FirebaseMessaging.onMessage.listen((event) {
-      widget.onNotificationReceived?.call(event);
-      var shouldShow = (widget.shouldShowNotification?.call(event) ?? true);
-      if (shouldShow) {
-        if (widget.showNotification != null) {
-          widget.showNotification!(event);
-        } else {
-          HelperFunctions.showNotifications(
-            event,
-            widget.androidSoundFile,
-            widget.channelKey,
-          );
-        }
-      }
-    });
+    FirebaseMessaging.onMessage.listen((msg) {
+      widget.onNotificationReceived?.call(msg);
 
-    _fcm.onTokenRefresh.listen(widget.onTokenRefresh);
+      _checkAndShowNotification(msg);
+    });
 
     if (widget.backgroundHandler != null) {
       FirebaseMessaging.onBackgroundMessage(widget.backgroundHandler!);
+    }
+
+    _fcm.onTokenRefresh.listen(widget.onTokenRefresh);
+  }
+
+  Future<void> _initJPush() async {
+    _jPush.setup(
+      appKey: widget.jpushAppKey ?? '',
+      channel: widget.channelKey ?? "default",
+    );
+
+    _jPush.applyPushAuthority(
+      const NotificationSettingsIOS(
+        sound: true,
+        alert: true,
+        badge: true,
+      ),
+    );
+
+    final rid = await _jPush.getRegistrationID();
+    widget.onGetToken?.call(rid);
+
+    _jPush.addEventHandler(
+      onReceiveNotification: (msg) async {
+        final converted = _convertToRemoteMessage(msg);
+        widget.onNotificationReceived?.call(converted);
+        _checkAndShowNotification(converted);
+      },
+      onOpenNotification: (msg) async {
+        final converted = _convertToRemoteMessage(msg);
+        widget.onNotificationReceived?.call(converted);
+        widget.onTap?.call(converted.data);
+      },
+      onReceiveMessage: (msg) async {
+        final converted = _convertToRemoteMessage(msg);
+        widget.onNotificationReceived?.call(converted);
+      },
+    );
+  }
+
+  RemoteMessage _convertToRemoteMessage(Map<String, dynamic> msg) {
+    return RemoteMessage(
+      data: Map<String, dynamic>.from(msg["extras"] ?? {}),
+      notification: RemoteNotification(
+        title: msg["title"]?.toString(),
+        body: msg["alert"]?.toString(),
+      ),
+    );
+  }
+
+  void _checkAndShowNotification(RemoteMessage message) {
+    final shouldShow = widget.shouldShowNotification?.call(message) ?? true;
+
+    if (shouldShow) {
+      if (widget.showNotification != null) {
+        widget.showNotification!(message);
+      } else {
+        HelperFunctions.showNotifications(
+          message,
+          widget.androidSoundFile,
+          widget.channelKey,
+        );
+      }
     }
   }
 
